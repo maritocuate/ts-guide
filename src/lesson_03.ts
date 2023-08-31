c9d4deae03e4225e314d01abbf608a99f1cd7b44
maritocuate <mario300@gmail.com>
2023-08-31 16:03:13 -0300

    lesson 3: arrays

---
+++ src/lesson_03.ts
@@ -0,0 +1,11 @@
+//  Lesson 03: Arrays
+
+const lettersA: string[] = ['a', 'b', 'c']
+
+interface Letters {
+  name: string
+}
+const lettersB: Letters[] = [{ name: 'Anna' }, { name: 'Betty' }]
+
+console.log(lettersA)
+console.log(lettersB)
