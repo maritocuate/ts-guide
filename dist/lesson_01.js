908954aaf44cacde5030cf47dd253719216393fb
maritocuate <mario300@gmail.com>
2023-08-31 15:48:58 -0300

    first commit

---
+++ dist/lesson_01.js
@@ -0,0 +1,10 @@
+"use strict";
+var message = 'Hello world';
+var decimal = 6;
+var thing = 'some';
+var list = [1, 2, 3];
+console.log(message);
+console.log(decimal);
+console.log(thing);
+console.log(list);
+//# sourceMappingURL=lesson_01.js.map
---
+++ src/lesson_01.ts
@@ -0,0 +1,11 @@
+//  Lesson 01: Literal annotation
+
+const message: string = 'Hello world'
+const decimal: number = 6
+const thing: any = 'some'
+const list: number[] = [1, 2, 3]
+
+console.log(message)
+console.log(decimal)
+console.log(thing)
+console.log(list)
